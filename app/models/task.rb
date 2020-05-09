dfa710760716505a6433a48b43f0bba933f3b909
katsuhiro.tsukinoki@gmail.com <slowandlow3104>
2020-05-09 12:26:48 +0900

    Add ラベル、ラベリングモデル

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,13 +1,16 @@
 class Task < ApplicationRecord
 
+  belongs_to :user, optional: true
+  has_many :labellings, dependent: :destroy
+  has_many :labels, through: :labellings
+
+  
   validates :name, presence: true
   validates :description, presence: true
   validates :end_date, presence: true
   validates :status, presence: true
   validates :priority, presence: true
   validate :pretend_ago
-
-  belongs_to :user, optional: true
 
   def pretend_ago
     errors.add(:end_date, ' 過去の日付は入力できません') if end_date.nil? || end_date < Date.today
