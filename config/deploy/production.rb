d28c05bb0755c49d76645a67eb1dfdc54cbce609
katsuhiro.tsukinoki@gmail.com <slowandlow3104>
2020-05-20 13:21:51 +0900

    init unicorn

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
-server '54.248.156.249', user: 'app', roles: %w{app db web}
+server '54.248.203.222', user: 'app', roles: %w{app db web}
 set :ssh_options, keys: '/Users/tsukinokikatsuhiro/.ssh/id_rsa'
